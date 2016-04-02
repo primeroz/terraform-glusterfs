ab2beac16a7d04e087b2a35b00b538738186a933
Francesco Ciocchetti <primeroznl@gmail.com>
2016-04-02 14:18:48 +0100

    Restructure provisioning for more parallelism
    Make it work so that it does create a gluster volume

--- terraform/provision.tf
+++ terraform/provision.tf
@@ -1,5 +1,9 @@
-resource "null_resource" "provision_gluster-client" {
-    depends_on = ["aws_instance.gluster-client","null_resource.provision_gluster01","null_resource.provision_gluster02"]
+#
+# BASE PROVISIONING
+#
+
+resource "null_resource" "base_gluster-client" {
+    depends_on = ["aws_instance.gluster-client"]
     triggers = {
         instance_id = "${aws_instance.gluster-client.id}"
     }
@@ -28,14 +32,15 @@ resource "null_resource" "provision_gluster-client" {
 
     provisioner "remote-exec" {
       inline = [
+        "chmod a+x /tmp/scripts/*.sh",
         "/usr/bin/sudo /tmp/001_base.sh"
       ]
     }
 
 }
 
-resource "null_resource" "provision_gluster01" {
-    depends_on = ["aws_instance.gluster01","null_resource.provision_gluster02"]
+resource "null_resource" "base_gluster01" {
+    depends_on = ["aws_instance.gluster01"]
     triggers = {
         instance_id = "${aws_instance.gluster01.id}"
     }
@@ -64,13 +69,14 @@ resource "null_resource" "provision_gluster01" {
 
     provisioner "remote-exec" {
       inline = [
-        "/usr/bin/sudo /tmp/001_base.sh"
+        "chmod a+x /tmp/scripts/*.sh",
+        "/usr/bin/sudo /tmp/001_base.sh",
       ]
     }
 
 }
 
-resource "null_resource" "provision_gluster02" {
+resource "null_resource" "base_gluster02" {
     depends_on = ["aws_instance.gluster02"]
     triggers = {
         instance_id = "${aws_instance.gluster02.id}"
@@ -100,7 +106,73 @@ resource "null_resource" "provision_gluster02" {
 
     provisioner "remote-exec" {
       inline = [
+        "chmod a+x /tmp/scripts/*.sh",
         "/usr/bin/sudo /tmp/001_base.sh",
+      ]
+    }
+
+}
+
+
+#
+# GLUSTER PROVISIONING
+#
+
+resource "null_resource" "provision_gluster-client" {
+    depends_on = ["null_resource.base_gluster-client","null_resource.provision_gluster01"]
+    triggers = {
+        instance_id = "${aws_instance.gluster-client.id}"
+    }
+    connection {
+        host = "${aws_instance.gluster-client.public_ip}"
+        user = "${var.ssh_username}"
+        key_file = "${var.ssh_keypath}"
+        agent = false
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "/usr/bin/sudo /tmp/scripts/010_gluster_client.sh"
+      ]
+    }
+
+}
+
+resource "null_resource" "provision_gluster01" {
+    depends_on = ["null_resource.base_gluster01","null_resource.provision_gluster02"]
+    triggers = {
+        instance_id = "${aws_instance.gluster01.id}"
+    }
+    connection {
+        host = "${aws_instance.gluster01.public_ip}"
+        user = "${var.ssh_username}"
+        key_file = "${var.ssh_keypath}"
+        agent = false
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "/usr/bin/sudo /tmp/scripts/010_gluster_server.sh",
+        "/usr/bin/sudo /tmp/scripts/015-gluster_cluster_setup.sh",
+      ]
+    }
+
+}
+
+resource "null_resource" "provision_gluster02" {
+    depends_on = ["null_resource.base_gluster02"]
+    triggers = {
+        instance_id = "${aws_instance.gluster02.id}"
+    }
+    connection {
+        host = "${aws_instance.gluster02.public_ip}"
+        user = "${var.ssh_username}"
+        key_file = "${var.ssh_keypath}"
+        agent = false
+    }
+
+    provisioner "remote-exec" {
+      inline = [
         "/usr/bin/sudo /tmp/scripts/010_gluster_server.sh"
       ]
     }
--- terraform/scripts/015-gluster_cluster_setup.sh
+++ terraform/scripts/015-gluster_cluster_setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -x
+set -e
 
 # This will only run on gluster01
 
-gluster peer probe gluster02
+if ! (gluster peer status | egrep -q gluster02); then
+	gluster peer probe gluster02
+	sleep 5
+fi
 
-gluster volume create test1 replica 2 transport tcp gluster01:/bricks/01 gluster02:/bricks/01 force
+if ! (gluster volume list | egrep -q test1); then
+	gluster volume create test1 replica 2 transport tcp gluster01:/bricks/01 gluster02:/bricks/01 force
+fi
 
-gluster status test1
+sleep 2
+gluster volume start test1 2>&1 >/dev/null || true
+
+gluster volume status test1
