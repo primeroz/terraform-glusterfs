fbf694fefb6a4dfd665561ee3d5111d829d10be8
Francesco Ciocchetti <primeroznl@gmail.com>
2016-04-02 14:18:28 +0100

    Add private and public ips terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,24 @@
-#output "vyos01_private_ip" {
-#  value = "${aws_instance.vyos01.private_ip}"
-#}
-#
-#output "vyos02_private_ip" {
-#  value = "${aws_instance.vyos02.private_ip}"
-#}
-#
-#output "vyos01_public_ip" {
-#  value = "${aws_instance.vyos01.public_ip}"
-#}
-#
-#output "vyos02_public_ip" {
-#  value = "${aws_instance.vyos02.public_ip}"
-#}
+output "gluster01_private_ip" {
+  value = "${aws_instance.gluster01.private_ip}"
+}
+
+output "gluster01_public_ip" {
+  value = "${aws_instance.gluster01.public_ip}"
+}
+
+output "gluster02_private_ip" {
+  value = "${aws_instance.gluster02.private_ip}"
+}
+
+output "gluster02_public_ip" {
+  value = "${aws_instance.gluster02.public_ip}"
+}
+
+output "gluster-client_private_ip" {
+  value = "${aws_instance.gluster-client.private_ip}"
+}
+
+output "gluster-client_public_ip" {
+  value = "${aws_instance.gluster-client.public_ip}"
+}
+
